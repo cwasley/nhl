e2553702991a7b056ec90af8dc51e53ebf7c28ee
Collin Wasley <collin.wasley@typelaw.com>
2018-04-09 09:50:31 -0700

    Updated to reflect weekend games

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,17 @@ var data = {
         "city": "Nashville",
         "mascot": "Predators",
         "color": "#FFB81C",
-        "pts": 115,
-        "row": 46,
+        "pts": 117,
+        "row": 47,
         "division": "central",
         "conference": "western",
         "seed": "(1)"
     },
     1 : {
-        "city": "St. Louis",
-        "mascot": "Blues",
-        "color": "#003087",
-        "pts": 94,
+        "city": "Colorado",
+        "mascot": "Avalanche",
+        "color": "#6F263D",
+        "pts": 95,
         "row": 41,
         "division": "central",
         "conference": "western",
@@ -28,8 +28,8 @@ var data = {
         "city": "Winnipeg",
         "mascot": "Jets",
         "color": "#041E42",
-        "pts": 112,
-        "row": 47,
+        "pts": 114,
+        "row": 48,
         "division": "central",
         "conference": "western",
         "seed": "(2)"
@@ -38,8 +38,8 @@ var data = {
         "city": "Minnesota",
         "mascot": "Wild",
         "color": "#154734",
-        "pts": 99,
-        "row": 41,
+        "pts": 101,
+        "row": 42,
         "division": "central",
         "conference": "western",
         "seed": "(3)"
@@ -65,21 +65,21 @@ var data = {
         "seed": "(WC1)"
     },
     6 : {
-        "city": "San Jose",
-        "mascot": "Sharks",
-        "color": "#006272",
-        "pts": 100,
+        "city": "Anaheim",
+        "mascot": "Ducks",
+        "color": "#FC4C02",
+        "pts": 101,
         "row": 40,
         "division": "pacific",
         "conference": "western",
         "seed": "(2)"
     },
     7 : {
-        "city": "Anaheim",
-        "mascot": "Ducks",
-        "color": "#FC4C02",
-        "pts": 99,
-        "row": 39,
+        "city": "San Jose",
+        "mascot": "Sharks",
+        "color": "#006272",
+        "pts": 100,
+        "row": 40,
         "division": "pacific",
         "conference": "western",
         "seed": "(3)"
@@ -88,53 +88,13 @@ var data = {
         "city": "Tampa Bay",
         "mascot": "Lightning",
         "color": "#00205B",
-        "pts": 112,
+        "pts": 113,
         "row": 48,
         "division": "atlantic",
         "conference": "eastern",
         "seed": "(1)"
     },
     9 : {
-        "city": "Philadelphia",
-        "mascot": "Flyers",
-        "color": "#FA4616",
-        "pts": 98,
-        "row": 40,
-        "division": "metropolitan",
-        "conference": "eastern",
-        "seed": "(WC2)"
-    },
-    10 : {
-        "city": "Boston",
-        "mascot": "Bruins",
-        "color": "#000000",
-        "pts": 110,
-        "row": 46,
-        "division": "atlantic",
-        "conference": "eastern",
-        "seed": "(2)"
-    },
-    11 : {
-        "city": "Toronto",
-        "mascot": "Maple Leafs",
-        "color": "#00205B",
-        "pts": 103,
-        "row": 41,
-        "division": "atlantic",
-        "conference": "eastern",
-        "seed": "(3)"
-    },
-    12 : {
-        "city": "Washington",
-        "mascot": "Capitals",
-        "color": "#041E42",
-        "pts": 103,
-        "row": 45,
-        "division": "metropolitan",
-        "conference": "eastern",
-        "seed": "(1)"
-    },
-    13 : {
         "city": "New Jersey",
         "mascot": "Devils",
         "color": "#C8102E",
@@ -143,6 +103,46 @@ var data = {
         "division": "metropolitan",
         "conference": "eastern",
         "seed": "(WC1)"
+    },
+    10 : {
+        "city": "Boston",
+        "mascot": "Bruins",
+        "color": "#000000",
+        "pts": 112,
+        "row": 47,
+        "division": "atlantic",
+        "conference": "eastern",
+        "seed": "(2)"
+    },
+    11 : {
+        "city": "Toronto",
+        "mascot": "Maple Leafs",
+        "color": "#00205B",
+        "pts": 105,
+        "row": 42,
+        "division": "atlantic",
+        "conference": "eastern",
+        "seed": "(3)"
+    },
+    12 : {
+        "city": "Washington",
+        "mascot": "Capitals",
+        "color": "#C8102E",
+        "pts": 105,
+        "row": 46,
+        "division": "metropolitan",
+        "conference": "eastern",
+        "seed": "(1)"
+    },
+    13 : {
+        "city": "Columbus",
+        "mascot": "Blue Jackets",
+        "color": "#041E42",
+        "pts": 97,
+        "row": 39,
+        "division": "metropolitan",
+        "conference": "eastern",
+        "seed": "(3)"
     },
     14 : {
         "city": "Pittsburgh",
@@ -155,14 +155,14 @@ var data = {
         "seed": "(2)"
     },
     15 : {
-        "city": "Columbus",
-        "mascot": "Blue Jackets",
-        "color": "#041E42",
-        "pts": 97,
-        "row": 39,
+        "city": "Philadelphia",
+        "mascot": "Flyers",
+        "color": "#FA4616",
+        "pts": 98,
+        "row": 40,
         "division": "metropolitan",
         "conference": "eastern",
-        "seed": "(3)"
+        "seed": "(WC2)"
     }
 };
 
