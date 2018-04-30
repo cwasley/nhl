ce169dd6f453b3864503421da73248e4cabff47d
Collin Wasley <collin.wasley@typelaw.com>
2018-04-30 16:33:23 -0700

    Fixed standings page for table to be correctly formatted. Modified brackets page to have striping on games that have already been played: angled left is wrong, angled right is correct.

--- public/javascripts/nhl.js
+++ public/javascripts/nhl.js
@@ -523,6 +523,11 @@ angular.module('nhl', [])
             return (angle < 120 || angle > 270) ? angle : angle + 180;
             //return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
         }
+        function shadeColor(color, percent) {
+            var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+            return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+        }
+
         function calculateBracket(nodeArray) {
 
             var nodeData = {
@@ -643,8 +648,14 @@ angular.module('nhl', [])
                     return '0';
                 })
                 .style("fill", function (d) {
-                    if (d.data.status > 0) {
-                        return d.data.color;
+                    if (d.data.status > 1) {
+                        var newTexture = textures.lines()
+                            .orientation("6/8").size(10).strokeWidth(4).background(d.data.color).stroke(shadeColor(d.data.color, 0.3));
+                        g.call(newTexture);
+                        return newTexture.url();
+                    }
+                    if (d.data.status === 1) {
+                        return shadeColor(d.data.color, 0.1);
                     }
                     if (d.data.status === 0) {
                         return 'gray';
